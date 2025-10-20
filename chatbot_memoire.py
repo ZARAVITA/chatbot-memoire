9e9ad2a49a21b7e6151376173570232c4dd49db4
ZARAVITA <zaravitamds18@gmail.com>
2025-10-20 17:24:30 +0100

    Ajout de script temporaire
    Added a function to check package versions.

--- chatbot_memoire.py
+++ chatbot_memoire.py
@@ -8,6 +8,14 @@ Technologies : LangChain, FAISS, Hugging Face, Streamlit
 
 import os
 import streamlit as st
+import pkgutil, importlib
+#TEMPORAIRE
+def try_version(pkg_name, import_name=None):
+    try:
+        m = importlib.import_module(import_name or pkg_name)
+        return getattr(m, "__version__", "installed (no __version__)")
+    except Exception as e:
+        return f"NOT INSTALLED / import error: {e}"
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
